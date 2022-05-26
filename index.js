849be4706c129a518f43a251169a6e2ec8795e71
Neamul01 <neamulhaque2002@gmail.com>
2022-05-26 18:22:05 +0600

    get all user api added

--- index.js
+++ index.js
@@ -55,6 +55,11 @@ async function run() {
         });
 
         //user api's here
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
