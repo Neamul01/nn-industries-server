e43c1899d7ff2123cdaafa75cf218a10ba8c376e
Neamul01 <neamulhaque2002@gmail.com>
2022-05-27 23:37:01 +0600

    admin role api added

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ async function run() {
             res.send({ result, token })
         })
 
+        //admin api's
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
         app.put('/users/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
